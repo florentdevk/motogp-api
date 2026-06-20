c8c6a1d9aa647664d478bb934409fce687148a33
florentdevk <florentdevk@gmail.com>
2026-06-20 19:45:23 +0200

    feat: expose entities via ApiResource

--- src/Entity/RaceResult.php
+++ src/Entity/RaceResult.php
@@ -4,7 +4,9 @@ namespace App\Entity;
 
 use App\Repository\RaceResultRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: RaceResultRepository::class)]
 class RaceResult
 {
